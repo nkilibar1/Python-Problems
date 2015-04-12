4f999e2c3f5704d26adbfddd02447f003edf6a48
nkilibar1 <nkilibar1@gmail.com>
2015-04-12 17:18:18 -0400

    Update tetris.py

--- tetris.py
+++ tetris.py
@@ -100,6 +100,15 @@ class Shape():
         for block in self.blocks:
             block.draw(win)
 
+    def undraw(self):
+        ''' Parameter: win - type: CanvasFrame
+
+            unDraws the shape:
+            i.e. draws each block
+        '''
+        for block in self.blocks:
+            block.undraw()
+
     def move(self, dx, dy):
         ''' Parameters: dx - type: int
                         dy - type: int
@@ -320,13 +329,12 @@ class Board():
             3. Returns True in all other cases
         '''
 
-        if  x not in range(Tetris.BOARD_WIDTH) or y not in range(Tetris.BOARD_HEIGHT):
+        if x not in range(Tetris.BOARD_WIDTH) or y not in range(Tetris.BOARD_HEIGHT):
             return False
         elif (x, y) in self.grid.keys():
             return False
         else:
             return True
-
 
     def add_shape(self, shape):
         ''' Parameter: shape - type:Shape
@@ -343,7 +351,6 @@ class Board():
         for block in blocks:
             self.grid[(block.x, block.y)] = block
 
-
     def delete_row(self, y):
         ''' Parameters: y - type:int
 
@@ -358,7 +365,6 @@ class Board():
         for x in range(10):
             self.grid[(x, y)].undraw()
             del self.grid[(x, y)]
-
 
     def is_row_complete(self, y):
         ''' Parameter: y - type: int
@@ -376,7 +382,6 @@ class Board():
 
         return True
 
-
     def move_down_rows(self, y_start):
         ''' Parameters: y_start - type:int
 
@@ -393,11 +398,10 @@ class Board():
                 if (x, y_start) in self.grid.keys():
                     block = self.grid[(x, y_start)]
                     del self.grid[(x, y_start)]
-                    block.move(0,1)
+                    block.move(0, 1)
                     self.grid[(block.x, block.y)] = block
 
             y_start -= 1
-
 
     def remove_complete_rows(self):
         ''' removes all the complete rows
@@ -413,16 +417,16 @@ class Board():
                 self.delete_row(y)
                 self.move_down_rows(y)
 
-
     def game_over(self):
         ''' display "Game Over !!!" message in the center of the board
             HINT: use the Text class from the graphics library
         '''
-        game = Text(Point(self.width/2*Block.BLOCK_SIZE, self.height/2*Block.BLOCK_SIZE), "Game Over !!!")
+        game = Text(Point(self.width / 2 * Block.BLOCK_SIZE, self.height / 2.5 *
+                                            Block.BLOCK_SIZE), "Game Over !!!")
         game.setSize(30)
         game.setStyle('bold')
         game.draw(self.canvas)
-
+        return True
 
 ############################################################
 # SCOREBOARD CLASS
@@ -435,22 +439,25 @@ class ScoreBoard():
         Attributes: width - type:int - width of the board in squares
                     height - type:int - height of the board in squares
                     canvas - type:CanvasFrame - where the pieces will be drawn
-                    grid - type:Dictionary - keeps track of the current state of
-                    the board; stores the blocks for a given position
+
     '''
     Level = 1
     Level2 = 1
     Display = 0
+
     def __init__(self, win, width, height):
         self.width = width
         self.height = height
         self.canvas = CanvasFrame(win, self.width * Block.BLOCK_SIZE,
                                         self.height * Block.BLOCK_SIZE)
         self.canvas.setBackground('light gray')
-        self.current_score = Text(Point(8*Block.BLOCK_SIZE, self.height/2*Block.BLOCK_SIZE), self.Display)
+        self.current_score = Text(Point(8 * Block.BLOCK_SIZE, self.height / 2 *
+                                                Block.BLOCK_SIZE), self.Display)
         self.current_score.setSize(25)
         self.current_score.draw(self.canvas)
-        self.current_level = Text(Point(self.width/4*Block.BLOCK_SIZE, self.height/2*Block.BLOCK_SIZE), "Level " + str(self.Level))
+        self.current_level = Text(Point(self.width / 4 * Block.BLOCK_SIZE,
+                                        self.height / 2 * Block.BLOCK_SIZE),
+                                        "Level " + str(self.Level))
         self.current_level.setSize(25)
         self.current_level.draw(self.canvas)
 
@@ -480,14 +487,47 @@ class ScoreBoard():
             self.Level2 += 1
             return True
 
+############################################################
+# SPIECE PREVIEW CLASS
+############################################################
 
+
+class PiecePreview():
+    ''' piece preview class: Shows the upcoming piece
+
+        Attributes: width - type:int - width of the board in squares
+                    height - type:int - height of the board in squares
+                    canvas - type:CanvasFrame - where the pieces will be drawn
+                    grid - type:Dictionary - keeps track of the current state of
+                    the board; stores the blocks for a given position
+    '''
+
+    def __init__(self, win, width, height):
+        self.width = width
+        self.height = height
+        self.canvas = CanvasFrame(win, self.width * Block.BLOCK_SIZE,
+                                        self.height * Block.BLOCK_SIZE)
+        self.canvas.setBackground('light gray')
+
+    def draw_shape(self, n):
+        ''' Parameters: shape - type: Shape
+            draws the preview piece on the board
+        '''
+        self.preview = Tetris.SHAPES[n](Point(int(Tetris.BOARD_WIDTH / 2), 0.5))
+        self.preview.draw(self.canvas)
+
+    def remove_shape(self):
+        ''' Removes the current piece preview
+
+        '''
+        Shape.undraw(self.preview)
 
 
 ############################################################
 # TETRIS CLASS
 ############################################################
 
-class Tetris(ScoreBoard):
+class Tetris(object):
     ''' Tetris class: Controls the game play
         Attributes:
             SHAPES - type: list (list of Shape classes)
@@ -501,19 +541,19 @@ class Tetris(ScoreBoard):
     '''
 
     SHAPES = [I_shape, J_shape, L_shape, O_shape, S_shape, T_shape, Z_shape]
-    DIRECTION = {'Left':(-1, 0), 'Right':(1, 0), 'Down':(0, 1)}
+    DIRECTION = {'Left': (-1, 0), 'Right': (1, 0), 'Down': (0, 1)}
     BOARD_WIDTH = 10
     BOARD_HEIGHT = 20
 
     def __init__(self, win):
 
-
         self.score = ScoreBoard(win, self.BOARD_WIDTH, 2)
+        self.piece = PiecePreview(win, self.BOARD_WIDTH, 3)
         self.board = Board(win, self.BOARD_WIDTH, self.BOARD_HEIGHT)
 
         self.win = win
-        self.delay = 1000 #ms
-
+        self.delay = 1000  # ms
+        self.key = 0
         # sets up the keyboard events
         # when a key is called the method key_pressed will be called
         self.win.bind_all('<Key>', self.key_pressed)
@@ -521,14 +561,13 @@ class Tetris(ScoreBoard):
         # set the current shape to a random new shape
         self.current_shape = self.create_new_shape()
 
-        # Draw the current_shape oan the board (take a look at the
+        # Draw the current_shape on the board (take a look at the
         # draw_shape method in the Board class)
         self.board.draw_shape(self.current_shape)
-
+        self.new = self.create_new_shape()
+        PiecePreview.draw_shape(self.piece, self.random_number)
         #  animate the shape!
         self.animate_shape()
-
-
 
     def create_new_shape(self):
         ''' Return value: type: Shape
@@ -537,101 +576,107 @@ class Tetris(ScoreBoard):
              at y = 0 and x = int(self.BOARD_WIDTH/2)
             return the shape
         '''
-
-        #return self.SHAPES[random.randint(0,6)](Point(int(self.BOARD_WIDTH/2),0))
-        return self.SHAPES[0](Point(int(self.BOARD_WIDTH/2),0))
+        self.random_number = random.randint(0, 6)
+        self.new_shape = self.SHAPES[self.random_number](Point(int
+                                        (self.BOARD_WIDTH / 2), 0))
+        return self.new_shape
 
     def animate_shape(self):
         ''' animate the shape - move down at equal intervals
             specified by the delay attribute
         '''
-
-        self.do_move('Down')
-        self.win.after(self.delay, self.animate_shape)
+        if (self.key == 'p' or self.key == 'P') and self.timestop != self.delay:
+            return
+        if self.current_shape.can_move(self.board, 0, 0):
+            self.do_move('Down')
+            self.win.after(self.delay, self.animate_shape)
 
     def do_move(self, direction):
         ''' Parameters: direction - type: string
             Return value: type: bool
 
             Move the current shape in the direction specified by the parameter:
-            First check if the shape can move. If it can, move it and return True
-            Otherwise if the direction we tried to move was 'Down',
-            1. add the current shape to the board, scores completed rows and checks if level up
-            2. remove the completed rows if any, create a new random shape and set current_shape attribute
-            4. If the shape cannot be drawn on the board, display a
+            First check if the shape can move. If it can, move it and return
+            True Otherwise if the direction we tried to move was 'Down',
+            add the current shape to the board, scores completed rows
+            checks if level up, remove the completed rows if any,
+            create a new random shape and sets current_shape attribute
+            3. If the shape cannot be drawn on the board, displays a
                game over message
 
-            return False
-
         '''
-        (dx,dy) = self.DIRECTION[direction]
+
+        (dx, dy) = self.DIRECTION[direction]
 
         if self.current_shape.can_move(self.board, dx, dy):
-            self.current_shape.move(dx,dy)
+            self.current_shape.move(dx, dy)
             return True
 
-        if self.current_shape.can_move(self.board, dx, dy) == False and (dx, dy) == (0, 1):
+        if self.current_shape.can_move(self.board, dx,
+                     dy) == False and (dx, dy) == (0, 1):
             Board.add_shape(self.board, self.current_shape)
-            scored1 = 5*ScoreBoard.Level
+            PiecePreview.remove_shape(self.piece)
+            scored1 = 5 * ScoreBoard.Level
             ScoreBoard.update(self.score, scored1)
             mult = 0
             for y in range(self.BOARD_HEIGHT):
-
                 if Board.is_row_complete(self.board, y) == True:
                     mult += 2
-                    print mult
             if mult > 0:
                 scored = 10 * mult * ScoreBoard.Level * mult
             elif mult >= 8:
                 scored = 1000 * ScoreBoard.Level
             else:
                 scored = 0
-            print scored
             ScoreBoard.update(self.score, scored)
-
             if ScoreBoard.level_up(self.score) == True:
                 self.delay -= 120
-
             Board.remove_complete_rows(self.board)
-
-            self.current_shape = self.create_new_shape()
+            self.current_shape = self.new
             self.board.draw_shape(self.current_shape)
+            self.new = self.create_new_shape()
+            PiecePreview.draw_shape(self.piece, self.random_number)
             return True
-
         return False
-
 
     def do_rotate(self):
         ''' Checks if the current_shape can be rotated and
             rotates if it can
         '''
+        if self.delay == 100000:
+            return
         if self.current_shape.can_rotate(self.board):
             self.current_shape.rotate(self.board)
 
     def key_pressed(self, event):
-        ''' this function is called when a key is pressed on the keyboard
-            it currenly just prints the value of the key
-
-            Modify the function so that if the user presses the arrow keys
-            'Left', 'Right' or 'Down', the current_shape will move in
+        ''' when a key is pressed on the keyboard the current_shape will move in
             the appropriate direction
-
             if the user presses the space bar 'space', the shape will move
             down until it can no longer move and is added to the board
-
-            if the user presses the 'Up' arrow key ,
-                the shape should rotate.
+            if the user presses the 'Up' arrow key
+                the shape rotates.
         '''
 
-        key = event.keysym
-        if key in self.DIRECTION:
-            return self.do_move(key)
-        elif key == 'space':
+        self.key = event.keysym
+        if self.key in self.DIRECTION:
+            if self.delay == 100000:
+                return
+            return self.do_move(self.key)
+        elif self.key == 'space':
+            if self.delay == 100000:
+                return
             while self.current_shape.can_move(self.board, 0, 1):
                 self.do_move('Down')
-        elif key == 'Up':
+        elif self.key == 'Up':
             self.do_rotate()
+        elif self.key == 'p' or self.key == 'P':
+            if self.delay == 100000 and (self.key == 'p' or self.key == 'P'):
+                self.delay = self.timestop
+                self.animate_shape()
 
+            else:
+                self.timestop = self.delay
+                self.delay = 100000
 
 ################################################################
 # Start the game
